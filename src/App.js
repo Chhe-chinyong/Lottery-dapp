1ea46231aeb02516eac2ef4ccbc500479bca9693
chinyong <chhe.chinyong@sabay.com>
2021-11-23 15:49:08 +0700

    first config with eslint

--- src/App.js
+++ src/App.js
@@ -1,201 +1,196 @@
-import { useEffect, useState, useRef } from "react";
-import lottery from "./lottery";
-import MetaMaskOnboarding from "@metamask/onboarding";
+import { useEffect, useState, useRef } from 'react';
+import lottery from './lottery';
+import MetaMaskOnboarding from '@metamask/onboarding';
 // import './App.css';
-import web3 from "./web3";
-var Accounts = require("web3-eth-accounts");
+import web3 from './web3';
 
 function App() {
-  const ONBOARD_TEXT = "Click here to install MetaMask!";
-  const CONNECT_TEXT = "Connect";
-  const CONNECTED_TEXT = "Connected";
+    const ONBOARD_TEXT = 'Click here to install MetaMask!';
+    const CONNECT_TEXT = 'Connect';
+    const CONNECTED_TEXT = 'Connected';
+    const [buttonText, setButtonText] = useState(ONBOARD_TEXT);
+    const [isDisabled, setDisabled] = useState(false);
+    const [accounts, setAccounts] = useState([]);
+    const onboarding = useRef();
 
-  const [buttonText, setButtonText] = useState(ONBOARD_TEXT);
-  const [isDisabled, setDisabled] = useState(false);
-  const [accounts, setAccounts] = useState([]);
-  const onboarding = useRef();
+    const [admin, setAdmin] = useState();
+    const [balance, setBalance] = useState('0');
 
-  const [admin, setAdmin] = useState();
-  const [lotterys, setLotterys] = useState();
-  const [balance, setBalance] = useState("0");
+    useEffect(() => {
+        if (!onboarding.current) {
+            onboarding.current = new MetaMaskOnboarding();
+        }
+    }, []);
 
-  useEffect(() => {
-    if (!onboarding.current) {
-      onboarding.current = new MetaMaskOnboarding();
+    useEffect(() => {
+        function handleNewAccounts(newAccounts) {
+            setAccounts(newAccounts);
+        }
+
+        const fetchData = async () => {
+            // const manager = await lottery.methods.players().call();
+            // const players = await lottery.methods.getPlayers().call();
+            // const balance = await web3.eth.getBalance(lottery.options.address);
+            // setLotterys(lotterys);
+            // lottery.defaultChain = 'rinkeby';
+            // const bobo = await web3.eth.givenProvider[1];
+            // console.log(bobo)
+            // let accounts = await web3.eth.getAccounts() ;
+            // console.log(accounts)
+            // console.log(await lottery.methods.admin().call());
+            // console.log(await lottery.methods.players(2).call());
+            // console.log(await lottery.methods)
+            const admin = await lottery.methods.admin().call();
+            // console.log(await lottery.methods.getBalance().call({from: admin})
+            const BalanceInPool = await lottery.methods.getBalance().call();
+            const BalanceInPoolConverted = web3.utils.fromWei(BalanceInPool, 'ether');
+            setBalance(BalanceInPoolConverted);
+            console.log('admin', admin);
+            console.log('hi');
+            // lottery.events.BalancePool()
+            // .on("data", function(event) {
+            //   let lottery = event.returnValues;
+            //   // We can access this event's 3 return values on the `event.returnValues` object:
+            //   console.log(event);
+            //   console.log("Currently balance in pool",  lottery.balance);
+            // }).on("error", console.error);
+            setAdmin(admin);
+        };
+        fetchData();
+
+        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+            window.ethereum.request({ method: 'eth_requestAccounts' }).then(handleNewAccounts);
+            window.ethereum.on('accountsChanged', handleNewAccounts);
+            return () => {
+                window.ethereum.removeListener('accountsChanged', handleNewAccounts);
+                // window.ethereum.off('accountsChanged', handleNewAccounts);
+            };
+        }
+    }, [admin]);
+
+    useEffect(() => {
+        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+            if (accounts.length > 0) {
+                setButtonText(CONNECTED_TEXT);
+                setDisabled(true);
+                onboarding.current.stopOnboarding();
+            } else {
+                setButtonText(CONNECT_TEXT);
+                setDisabled(false);
+            }
+        }
+    }, [accounts]);
+
+    //subscribe to event
+    lottery.events
+        .BalancePool()
+        .on('connected', function () {
+            console.log('connected to smart contract');
+        })
+        .on('data', function (event) {
+            let lottery = event.returnValues;
+            console.log(event);
+            let balance = lottery.balance;
+            let toEth = web3.utils.fromWei(balance, 'ether');
+            // We can access this event's 3 return values on the `event.returnValues` object:
+            console.log('Currently balance in pool', toEth);
+            setBalance(toEth);
+        })
+        .on('error', console.error);
+
+    async function handleOnclick() {
+        try {
+            const account = await web3.eth.accounts[0];
+            const fromAddress = await web3.eth.getAccounts();
+            console.log('from', fromAddress);
+            console.log('account', account);
+            await lottery.methods.pickWinner().send({ from: fromAddress[0] });
+        } catch (error) {
+            console.log(error);
+            const json = JSON.parse(error.Error);
+            console.log(json);
+        }
     }
-  }, []);
 
-  useEffect(() => {
-    function handleNewAccounts(newAccounts) {
-      setAccounts(newAccounts);
-    }
-
-    const fetchData = async () => {
-      // const manager = await lottery.methods.players().call();
-      // const players = await lottery.methods.getPlayers().call();
-      // const balance = await web3.eth.getBalance(lottery.options.address);
-      // setLotterys(lotterys);
-      // lottery.defaultChain = 'rinkeby';
-      // const bobo = await web3.eth.givenProvider[1];
-      // console.log(bobo)
-      // let accounts = await web3.eth.getAccounts() ;
-      // console.log(accounts)
-      // console.log(await lottery.methods.admin().call());
-      // console.log(await lottery.methods.players(2).call());
-      // console.log(await lottery.methods)
-      const admin = await lottery.methods.admin().call();
-      // console.log(await lottery.methods.getBalance().call({from: admin})
-      const BalanceInPool = await lottery.methods.getBalance().call();
-      const BalanceInPoolConverted = web3.utils.fromWei(BalanceInPool, "ether")
-      setBalance(BalanceInPoolConverted)
-      console.log("admin", admin);
-      console.log('hi')
-      // lottery.events.BalancePool()
-      // .on("data", function(event) {
-      //   let lottery = event.returnValues;
-      //   // We can access this event's 3 return values on the `event.returnValues` object:
-      //   console.log(event);
-      //   console.log("Currently balance in pool",  lottery.balance);
-      // }).on("error", console.error);
-      setAdmin(admin);
+    const onClick = () => {
+        if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+            web3.ethereum.request({ method: 'eth_requestAccounts' }).then((newAccounts) => setAccounts(newAccounts));
+        } else {
+            onboarding.current.startOnboarding();
+        }
     };
-    fetchData();
 
-    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then(handleNewAccounts);
-      window.ethereum.on("accountsChanged", handleNewAccounts);
-      return () => {
-        window.ethereum.removeListener("accountsChanged", handleNewAccounts);
-        // window.ethereum.off('accountsChanged', handleNewAccounts);
-      };
-    }
-  }, [admin]);
+    const handleOnJoin = async () => {
+        try {
+            const account = await web3.eth.accounts[0];
+            const fromAddress = await web3.eth.getAccounts();
+            console.log('from', fromAddress);
+            console.log('account', account);
+            const amount = '1';
 
-  useEffect(() => {
-    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-      if (accounts.length > 0) {
-        setButtonText(CONNECTED_TEXT);
-        setDisabled(true);
-        onboarding.current.stopOnboarding();
-      } else {
-        setButtonText(CONNECT_TEXT);
-        setDisabled(false);
-      }
-    }
-  }, [accounts]);
+            const amountToSend = web3.utils.toWei(amount, 'ether'); // Convert to wei value
+            // using the callback
+            web3.eth.sendTransaction(
+                {
+                    from: fromAddress[0],
+                    to: '0x4F028A0f6AC60CEB8E2303E76623b7f2BA3f5764',
+                    // chain: "ropsten",
+                    value: amountToSend,
+                    chainId: 3,
+                },
+                function (error, hash) {
+                    console.log(hash);
+                    console.log(error);
+                },
+            );
+        } catch (error) {
+            console.log(error);
+            const json = JSON.parse(error.Error);
+            console.log(json);
+        }
+    };
 
-  //subscribe to event
-  lottery.events
-    .BalancePool()
-    .on("connected", function (event) {
-      console.log("connected to smart contract");
-    })
-    .on("data", function (event) {
-      let lottery = event.returnValues;
-      console.log(event);
-      let balance = lottery.balance;
-      let toEth = web3.utils.fromWei(balance, "ether");
-      // We can access this event's 3 return values on the `event.returnValues` object:
-      console.log("Currently balance in pool", toEth);
-      setBalance(toEth);
-    })
-    .on("error", console.error);
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1 style={{ fontSize: '2.5rem' }}>Lottery by smart contract</h1>
+            <h2>Total Reward: {balance === '0' ? '0' : balance} ETH 💰💰</h2>
 
-  async function handleOnclick(e) {
-    try {
-      const account = await web3.eth.accounts[0];
-      const fromAddress = await web3.eth.getAccounts();
-      console.log("from", fromAddress);
-      console.log("account", account);
-      await lottery.methods.pickWinner().send({ from: fromAddress[0] });
-    } catch (error) {
-      console.log(error);
-      const json = JSON.parse(error.Error);
-      console.log(json);
-    }
-  }
+            <h2>
+                Admin{' '}
+                <a
+                    href="https://ropsten.etherscan.io/address/0xac56f689B36dC736301b3C5FAE880879057Ff65f"
+                    target={'_blank'}
+                    rel="noreferrer"
+                >
+                    {!admin ? 'Loading' : admin}{' '}
+                </a>{' '}
+            </h2>
+            {/* Participant with smart contract */}
+            <button onClick={handleOnJoin} style={{ padding: '0.5rem 1rem', margin: '1rem', fontWeight: 'bold' }}>
+                Partipate
+            </button>
 
-  const onClick = () => {
-    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-      web3.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((newAccounts) => setAccounts(newAccounts));
-    } else {
-      onboarding.current.startOnboarding();
-    }
-  };
+            {/* Button for admin */}
+            <button onClick={handleOnclick} style={{ padding: '0.5rem 1rem', margin: '1rem', fontWeight: 'bold' }}>
+                pick the winner
+            </button>
 
-  const handleOnJoin = async () => {
-    try {
-      const account = await web3.eth.accounts[0];
-      const fromAddress = await web3.eth.getAccounts();
-      console.log("from", fromAddress);
-      console.log("account", account);
-      const amount = "1";
-
-      const amountToSend = web3.utils.toWei(amount, "ether"); // Convert to wei value
-      // using the callback
-      web3.eth.sendTransaction({
-        from: fromAddress[0],
-        to: '0x4F028A0f6AC60CEB8E2303E76623b7f2BA3f5764',
-        // chain: "ropsten",
-        value: amountToSend,
-        chainId: 3
-      }, function (error, hash) {
-        console.log(hash);
-        console.log(error)
-      });
-    } catch (error) {
-      console.log(error);
-      const json = JSON.parse(error.Error);
-      console.log(json);
-    }
-
-
-  }
-
-  return (
-    <div style={{ textAlign: "center", }}>
-      <h1 style={{ fontSize: "2.5rem" }}>Lottery by smart contract</h1>
-      <h2>Total Reward: {balance === "0" ? "0" : balance} ETH 💰💰</h2>
-
-      <h2>
-        Admin{" "}
-        <a
-          href="https://ropsten.etherscan.io/address/0xac56f689B36dC736301b3C5FAE880879057Ff65f"
-          target={"_blank"}
-          rel="noreferrer"
-        >
-          {!admin ? "Loading" : admin}{" "}
-        </a>{" "}
-      </h2>
-      {/* Participant with smart contract */}
-      <button onClick={handleOnJoin} style={{ padding: "0.5rem 1rem", margin: "1rem", fontWeight: "bold" }}>Partipate</button>
-
-      {/* Button for admin */}
-      <button onClick={handleOnclick} style={{ padding: "0.5rem 1rem", margin: "1rem", fontWeight: "bold" }}>pick the winner</button>
-
-      <button disabled={isDisabled} onClick={onClick} style={{ padding: "0.5rem 1rem", margin: "1rem", fontWeight: "bold" }} >
-        {buttonText}
-      </button>
-      <br />
-      <ul style={{ display: "inline-block" }}>
-        <p style={{ fontWeight: "bolder", color: "#f5222d" }}>RULE to participate in lottery: </p>
-        <li>
-          All players need 1ETH in order to play this lottery
-        </li>
-        <li>
-          The winner of the prize is randomly gernerated by the computer
-        </li>
-        <li>
-          The winner take 90% the pool prize, and share 10% for developer.
-        </li>
-      </ul>
-
-    </div>
-  );
+            <button
+                disabled={isDisabled}
+                onClick={onClick}
+                style={{ padding: '0.5rem 1rem', margin: '1rem', fontWeight: 'bold' }}
+            >
+                {buttonText}
+            </button>
+            <br />
+            <ul style={{ display: 'inline-block' }}>
+                <p style={{ fontWeight: 'bolder', color: '#f5222d' }}>RULE to participate in lottery: </p>
+                <li>All players need 1ETH in order to play this lottery</li>
+                <li>The winner of the prize is randomly gernerated by the computer</li>
+                <li>The winner take 90% the pool prize, and share 10% for developer.</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/lottery.js
+++ src/lottery.js
@@ -1,164 +1,81 @@
-import web3 from "./web3";
-// const address ='0xf8E2db4541918e2Ed96450Cd91474fF3971b117c';
-// const address = "0xA07511588c0d5B9dDb63A0d14B5E53Da06E25166";
-const address = "0x4F028A0f6AC60CEB8E2303E76623b7f2BA3f5764";
-
-// const abi =
-// [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
-// const abi = [
-//   {
-//     inputs: [],
-//     stateMutability: "nonpayable",
-//     type: "constructor",
-//   },
-//   {
-//     anonymous: false,
-//     inputs: [
-//       {
-//         indexed: false,
-//         internalType: "uint256",
-//         name: "balance",
-//         type: "uint256",
-//       },
-//     ],
-//     name: "BalancePool",
-//     type: "event",
-//   },
-//   {
-//     inputs: [],
-//     name: "admin",
-//     outputs: [
-//       {
-//         internalType: "address",
-//         name: "",
-//         type: "address",
-//       },
-//     ],
-//     stateMutability: "view",
-//     type: "function",
-//   },
-//   {
-//     inputs: [],
-//     name: "getBalance",
-//     outputs: [
-//       {
-//         internalType: "uint256",
-//         name: "",
-//         type: "uint256",
-//       },
-//     ],
-//     stateMutability: "view",
-//     type: "function",
-//   },
-//   {
-//     inputs: [],
-//     name: "pickWinner",
-//     outputs: [],
-//     stateMutability: "nonpayable",
-//     type: "function",
-//   },
-//   {
-//     inputs: [
-//       {
-//         internalType: "uint256",
-//         name: "",
-//         type: "uint256",
-//       },
-//     ],
-//     name: "players",
-//     outputs: [
-//       {
-//         internalType: "address payable",
-//         name: "",
-//         type: "address",
-//       },
-//     ],
-//     stateMutability: "view",
-//     type: "function",
-//   },
-//   {
-//     stateMutability: "payable",
-//     type: "receive",
-//   },
-// ];
+import web3 from './web3';
+const address = '0x4F028A0f6AC60CEB8E2303E76623b7f2BA3f5764';
 // Local instance of Contract
-
 const abi = [
-  {
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "balance",
-        "type": "uint256"
-      }
-    ],
-    "name": "BalancePool",
-    "type": "event"
-  },
-  {
-    "inputs": [],
-    "name": "admin",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getBalance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "pickWinner",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "players",
-    "outputs": [
-      {
-        "internalType": "address payable",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "stateMutability": "payable",
-    "type": "receive"
-  }
+    {
+        inputs: [],
+        stateMutability: 'nonpayable',
+        type: 'constructor',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'balance',
+                type: 'uint256',
+            },
+        ],
+        name: 'BalancePool',
+        type: 'event',
+    },
+    {
+        inputs: [],
+        name: 'admin',
+        outputs: [
+            {
+                internalType: 'address',
+                name: '',
+                type: 'address',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'getBalance',
+        outputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'pickWinner',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        inputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+            },
+        ],
+        name: 'players',
+        outputs: [
+            {
+                internalType: 'address payable',
+                name: '',
+                type: 'address',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        stateMutability: 'payable',
+        type: 'receive',
+    },
 ];
-
 
 export default new web3.eth.Contract(abi, address);
